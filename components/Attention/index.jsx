6886e3338e038e81b0233bcf5a1357192f3e138f
maxinth <tunde.j.adeleye@gmail.com>
2022-03-18 12:56:28 +0100

    adding styles

--- components/Attention/index.jsx
+++ components/Attention/index.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-
+import styles from "./attention.module.scss";
 const MainAttention = () => {
   return (
-    <section>
-      <h1>Be the FIRST to Know!</h1>
+    <section className={styles.container}>
+      <h1 className={styles.mainHeading}>Be the FIRST to Know!</h1>
       <h2>
         Are you ready to experience a mind-blowing platform made just for you to
         sell all your content and digital products across borders, and massively
