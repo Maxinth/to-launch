a2f616077653a5e6a01e093e8837b9d77f4718bb
maxinth <tunde.j.adeleye@gmail.com>
2022-03-18 11:51:27 +0100

    logo

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import styles from "../styles/home.module.css";
+import styles from "styles/home.module.scss";
 
 export default function Home() {
   return <div className={styles.container}>clean slate protocol</div>;
