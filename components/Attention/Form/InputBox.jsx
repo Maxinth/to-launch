7af7d31e348b7764ca218ad8c7010d4d6be3e849
maxinth <tunde.j.adeleye@gmail.com>
2022-03-21 11:34:11 +0100

    added feedback comp

--- components/Attention/Form/InputBox.jsx
+++ components/Attention/Form/InputBox.jsx
@@ -4,7 +4,6 @@ import styles from "./form.module.scss";
 const InputBox = ({ type, placeholder }) => {
   return (
     <div className={styles.inputContainer}>
-      {/* <label htmlFor="">{label}</label> */}
       <input type={type} placeholder={placeholder} />
     </div>
   );
--- components/Attention/Form/index.jsx
+++ components/Attention/Form/index.jsx
@@ -1,15 +1,24 @@
-import React from "react";
+// import {useState} from "react";
 import InputBox from "./InputBox";
 import styles from "./form.module.scss";
+import cogoToast from "cogo-toast";
 
 const Form = () => {
+  const options = {
+    hideAfter: "5",
+    position: "top-right",
+  };
+  const handleSubmit = () => {
+    cogoToast.success("You have been added to the wait-list", options);
+  };
+
   return (
     <form className={styles.formMain}>
       <div className={styles.formBox}>
         <InputBox type="text" placeholder="Enter your First Name" />
         <InputBox type="mail" placeholder="Enter your Mail Address" />
       </div>
-      <button type="button" className={styles.btn}>
+      <button type="button" className={styles.btn} onClick={handleSubmit}>
         Join the wait-list
       </button>
     </form>
